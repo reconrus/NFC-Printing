6f59323d2e10b360bdde797b9f096dfaccfad89e
SelkieAnna <monstrolog@gmail.com>
2018-07-06 00:29:09 +0300

    first commit

---
+++ server/main.py
@@ -0,0 +1,17 @@
+from flask import Flask, request
+app = Flask(__name__)
+from printing import print_all
+
+@app.route("/", methods=['POST'])
+def print():
+    card_id = request.values.get("card_id")
+    readed_id = request.values.get("reader_id")
+    # find user and printer in the database:
+    # user_name = ...
+    # server = ...
+    # printer_name = ...
+    print_all(user_name, server, printer_name)
+    return "OK"
+
+if __name__ == '__main__':
+main()
