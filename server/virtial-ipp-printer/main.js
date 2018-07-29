b28bf4ffb1b072120db1d24d9e890cb36bacef8a
Yaroslav Yudinskikh <35652747+SimonWT@users.noreply.github.com>
2018-07-29 16:12:32 +0300

    ipp-server code

---
+++ server/virtial-ipp-printer/main.js
@@ -0,0 +1,28 @@
+var fs = require('fs')
+var Printer = require('ipp-printer')
+
+
+var config = require('rc')('ipp-printer', {
+  name: 'ipp-printer', dir: process.cwd(), port: 3000
+})
+ 
+var printer = new Printer(config)
+
+
+printer.on('job', function (job) {
+  console.log('[job %d] Printing document: %s', job.id, job.name)
+ 
+  var filename = 'job-' + job.id + '-' + Date.now() + '.ps' // .ps = PostScript
+  var file = fs.createWriteStream(filename)
+ 
+  job.on('end', function () {
+    console.log('[job %d] Document saved as %s', job.id, filename)
+  })
+ 
+  job.pipe(file)
+})
+
+printer.server.on('listening', function(){
+  console.log('Server starts at %s', printer.port)
+})
+
