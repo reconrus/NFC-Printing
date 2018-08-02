43513606e372878d6b18b20aee7c072fc6ba60c8
Yaroslav Yudinskikh <35652747+SimonWT@users.noreply.github.com>
2018-08-02 15:19:37 +0300

    Save jobs at Card_Id folders

--- server/virtial-ipp-printer/main.js
+++ server/virtial-ipp-printer/main.js
@@ -5,24 +5,34 @@ var Printer = require('ipp-printer')
 var config = require('rc')('ipp-printer', {
   name: 'ipp-printer', dir: process.cwd(), port: 3000
 })
- 
+
 var printer = new Printer(config)
 
 
 printer.on('job', function (job) {
   console.log('[job %d] Printing document: %s', job.id, job.name)
- 
+
+
+  var dir = 'C:\\docs_for_print\\'+printer.userId
+  try {
+    fs.mkdirSync(dir)
+  } catch (err) {
+    if (err.code !== 'EEXIST') throw err
+  }
+  
   var filename = 'job-' + job.id + '-' + Date.now() + '.ps' // .ps = PostScript
-  var file = fs.createWriteStream(filename)
- 
+  var file = fs.createWriteStream(dir+'\\'+filename)
+  console.log('UserID = ', printer.userId)
+
   job.on('end', function () {
     console.log('[job %d] Document saved as %s', job.id, filename)
+
   })
- 
+
   job.pipe(file)
 })
 
 printer.server.on('listening', function(){
+
   console.log('Server starts at %s', printer.port)
 })
-
