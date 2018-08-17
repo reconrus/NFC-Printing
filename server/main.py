4825b56b36661d6489ff24fd184a8c4e17150acd
SelkieAnna <monstrolog@gmail.com>
2018-08-17 12:49:27 +0300

    change printing to loogging inside main.py

--- server/main.py
+++ server/main.py
@@ -1,10 +1,15 @@
 from http.server import HTTPServer, BaseHTTPRequestHandler
-# import os
+import logging
+
 from flask import Flask, request
+
 from db_printer_info import getPrinterInfo, getCardId
 from Printing import StartPrint
+
 app = Flask(__name__)
-        
+logger = logging.getLogger(__name__)
+logger.setLevel(logging.INFO)
+
 @app.route("/print/", methods=['POST'])
 def nfc_reader_request_handling():
     '''
@@ -16,7 +21,7 @@ def nfc_reader_request_handling():
     reader_id = request.get_json()['reader_id']
     macPrinter = getPrinterInfo(reader_id)[0]
 
-    print("Card ID: " + card_id + "; NFC Reader ID: " + reader_id + "; Printer Name: " + macPrinter)
+    logger.info("Card ID: %s; NFC Reader ID: %s; Printer Name: %s" % (card_id,reader_id,macPrinter))
 
     StartPrint(printer=macPrinter, path=r'C:\\docs_for_print\\' + card_id)
     return "OK"
